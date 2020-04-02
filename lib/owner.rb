ea06b3db7d3b9206fe9ec9e9b4016a7fe589710e
Brandon Yoon <byoon5397@gmail.com>
2020-04-02 15:54:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,22 @@
 class Owner
+
   attr_reader :name, :species
-  def initialize(name, species)
+  @@all = []
+  def initialize(name)
     @name = name
     @species = "human"
+    Owner.all << self
   end
 
   def say_species
     "I am a #{@species}"
+  end
+
+  def self.all 
+    @@all?
+  end 
+
+  def self.count
+    all.count
   end
 end
